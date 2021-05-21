553c134928b186866df1e23b7dbb990d42a520dc
zakaria-altammo <zakaria.altammo@gmail.com>
2021-05-21 14:23:53 +0200

    form validation

--- form-view.php
+++ form-view.php
@@ -10,8 +10,57 @@
     <title>Order food & drinks</title>
 </head>
 <body>
+<?php
+$emailErr = $streetErr = $streetnumberErr = $cityErr = $zipcodeErr = "";
+$email = $street = $streetnumber = $city = $zipcode =  "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["email"])) {
+        $emailErr = "*Email is required";
+    } else {
+        $email = test_input($_POST["email"]);
+    }
+
+    if (empty($_POST["street"])) {
+        $streetErr = "*Street name is required";
+    } else {
+        $street = test_input($_POST["street"]);
+    }
+
+    if (empty($_POST["streetnumber"])) {
+        $streetnumberErr = "*Street number is required";
+    } else {
+        $streetnumber = test_input($_POST["streetnumber"]);
+    }
+    if (empty($_POST["city"])) {
+        $cityErr = "*City name is required";
+    } else {
+        $city = test_input($_POST["city"]);
+    }
+    if (empty($_POST["zipcode"])) {
+        $zipcodeErr = "*Zip code is required";
+    } else {
+        $zipcode = test_input($_POST["zipcode"]);
+    }
+}
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
 <div class="container">
     <h1>Order food in restaurant "the Personal Ham Processors"</h1>
+    <div class="alert alert-success" role="alert">
+
+        <span class="error"><?php echo "$emailErr";?></span><br>
+        <span class="error"><?php echo "$streetErr";?></span><br>
+
+        <span class="error"><?php echo "$cityErr";?></span><br>
+
+        <span class="error"><?php echo "$streetnumberErr";?></span><br>
+        <span class="error"><?php echo "$zipcodeErr";?></span>
+    </div>
     <nav>
         <ul class="nav">
             <li class="nav-item">
@@ -22,11 +71,14 @@
             </li>
         </ul>
     </nav>
-    <form method="post">
+
+
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="email">E-mail:</label>
-                <input type="text" id="email" name="email" class="form-control"/>
+                <input type="email" id="email" name="email" class="form-control" value="<?php echo $email?>"/>
+
             </div>
             <div></div>
         </div>
@@ -37,21 +89,25 @@
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="street">Street:</label>
-                    <input type="text" name="street" id="street" class="form-control">
+                    <input type="text" name="street" id="street" class="form-control" value="<?php echo $street?>">
+
                 </div>
                 <div class="form-group col-md-6">
                     <label for="streetnumber">Street number:</label>
-                    <input type="text" id="streetnumber" name="streetnumber" class="form-control">
+                    <input type="number" id="streetnumber" name="streetnumber" class="form-control" value="<?php echo $streetnumber?>">
+
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="city">City:</label>
-                    <input type="text" id="city" name="city" class="form-control">
+                    <input type="text" id="city" name="city" class="form-control" value="<?php echo $city?>">
+
                 </div>
                 <div class="form-group col-md-6">
                     <label for="zipcode">Zipcode</label>
-                    <input type="text" id="zipcode" name="zipcode" class="form-control">
+                    <input type="number" id="zipcode" name="zipcode" class="form-control" value="<?php echo $zipcode?>">
+
                 </div>
             </div>
         </fieldset>
@@ -64,6 +120,8 @@
                     &euro; <?php echo number_format($product['price'], 2) ?></label><br />
             <?php endforeach; ?>
         </fieldset>
+
+        </fieldset>
         
         <label>
             <input type="checkbox" name="express_delivery" value="5" /> 
@@ -75,8 +133,18 @@
 
     <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> in food and drinks.</footer>
 </div>
+<?php
+echo $email;
+echo $street;
+echo $streetnumber;
+echo $zipcode;
+echo $city;
 
+?>
 <style>
+    .error {
+        color: red;
+    }
     footer {
         text-align: center;
     }
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 //this line makes PHP behave in a more strict way
 declare(strict_types=1);
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
 
 //we are going to use session variables so we need to enable sessions
 session_start();
